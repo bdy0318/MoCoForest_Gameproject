3b52031b587c5e303d259005a14cb8ff05771ecf
nk457t <dess4713@sookmyung.ac.kr>
2022-01-13 19:11:41 +0900

    npc + inventory

--- Assets/1 Scripts/Follow.cs
+++ Assets/1 Scripts/Follow.cs
@@ -6,32 +6,9 @@ public class Follow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-    [SerializeField]
-    Player targetPlayer;
-    [SerializeField]
-    Shop shop;
-
-    public GameObject enterCamera;
-    public GameObject shoppingCamera;
-    //Vector3 EnterPosition = new Vector3(3.13f, 2.45f, 2.16f);
-    //Vector3 ShoppingPosition = new Vector3();
 
     void Update()
     {
-        if (targetPlayer.isShopping)
-        {
-            enterCamera.SetActive(true);
-        }
-        else if (shop.isEnter)
-        {
-            shoppingCamera.SetActive(true);
-        }
-        else
-        {
-            enterCamera.SetActive(false);
-            shoppingCamera.SetActive(false);
-            transform.position = target.position + offset; // 카메라 이동
-        }
-
+        transform.position = target.position + offset; // 카메라 이동
     }
 }
